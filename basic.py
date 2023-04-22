bde540ae1e9aa6f4ac78fe2af7acb2c681658a75
qaheri <ilya.15.ghaheri@gmail.com>
2023-04-22 22:55:36 +0330

    Update basic.py

--- basic.py
+++ basic.py
@@ -7,13 +7,13 @@ import datetime
 sudo_password=""
 u=input("enter username: ")
 e=int(input("enter expire: "))
-def create_user(username, expire):
+def create_user(username,expire):
     # inn = str(input("username,password:\n"))
     letters = string.digits
     result_str = ''.join(random.choice(letters) for i in range(8))
     
     password = result_str
-    
+    print(f"password: {password}")
     # command = "useradd -p "+password+" "+username+" --shell=/bin/false"
     # command = command.split()
 
@@ -22,22 +22,25 @@ def create_user(username, expire):
     # output = cmd2.stdout.read().decode()
     # print(output)
 
-    subprocess.run(['sudo', 'useradd', '-p', password, username, '--shell=/bin/false'])
+    subprocess.run(['sudo', 'useradd', username, '--shell=/bin/false'])
 
     expiration_date = datetime.date.today() + datetime.timedelta(days=expire)
 
-# Format the expiration date as YYYY-MM-DD
     expiration_date_str = expiration_date.strftime('%Y-%m-%d')
 
-# Run the chage command with sudo to set the expiration date for the account
+    p = subprocess.Popen(['sudo', 'passwd', username], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, err = p.communicate(input=bytes(password + '\n' + password + '\n', 'utf-8'))
+
+    print(output.decode('utf-8'))
+    print(err.decode('utf-8'))
+
     result = subprocess.run(['sudo', 'chage', '-E', expiration_date_str, username])
 
     if result.returncode == 0:
-        print(f"Successfully disabled account '{username}' after 30 days.")
+        print(f"Successfully disabled account '{username}' after {expire} days.")
     else:
         print(f"Failed to disable account '{username}':")
         print(result.stderr)
 
-    print(f"password: {password}")
 
-create_user(username=u, expire=e)
+create_user(username=u,expire=e)
